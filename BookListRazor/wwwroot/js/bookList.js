7b70b723f2337f2f97dbd6517af5d8a850bcb53f
Sandhia MLeod <mcleodsandhia@gmail.com>
2020-08-26 14:46:40 -0400

    Playing with controller

---
+++ BookListRazor/wwwroot/js/bookList.js
@@ -0,0 +1,36 @@
+﻿//var dataTable;
+
+//$(document).ready(function () {
+
+//    loadDataTable();
+//});
+
+
+//function loadDataTable() {
+//    dataTable = $('#DT_load').dataTable({
+//        "ajax": {
+//            "url": "/api/book",
+//            "type": "GET",
+//            "datatype": "json"
+//        },
+//        "columns": [
+//            { "data": "name", "width":"30%"},
+//            { "data": "author", "width": "30%" },
+//            { "data": "isbn", "width": "30%" },
+//            {
+//                "data": "id",
+//                "render": function (data) {
+//                    return '<div class="text-center"> 
+//                        <a href = "/BookList/Edit?id=${data}" class='btn btn-success text-white' style = 'cursor:pointer'; width: 100px; '>
+//                                Edit
+//                        </a>
+
+//                    </div>
+
+//                    '
+//                }
+//            }
+//        ]
+
+//    })
+//}
